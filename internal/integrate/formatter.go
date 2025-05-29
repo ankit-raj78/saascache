e460b002bd430d3d5cadddc354c8cbfb107c107f
ankit-raj78 <ankit.raj78800@gmail.com>
2025-05-29 11:23:18 -0400

    lol .\

--- internal/integrate/formatter.go
+++
@@ -1,9 +0,0 @@
-package integrate
-
-// Formatter handles integration formatting for various platforms.
-type Formatter struct{}
-
-// TODO: Implement integration formatting logic
-func (f *Formatter) Format() string {
-    return "formatted output"
-}
--- internal/recommend/engine.go
+++
@@ -1,9 +0,0 @@
-package recommend
-
-// Engine provides cache recommendations based on usage patterns.
-type Engine struct{}
-
-// TODO: Implement recommendation logic
-func (e *Engine) Recommend() string {
-    return "cache-aside"
-}
